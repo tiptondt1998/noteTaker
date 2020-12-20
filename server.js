e2a5f39a2a5341a6c6da90694ac7400b9d969d77
tiptondt1998 <dylan.tipton@live.com>
2020-12-20 11:02:30 -0500

    created directories:

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const PORT = 3001;
+const { v4 : uuidv4 }= require('uuid');
+
+let notes = require('./db/db.json');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/notes.html"))
+})
+
+app.get('/api/notes', (req, res)=>{
+    res.json(notes);
+    console.log(notes);
+})
+
+app.post('/api/notes', (req,res)=>{
+    let results = req.body;
+    let id=uuidv4();
+    results.id=id;
+    notes.push(results);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    console.log(results);
+    res.json(notes);
+});
+app.listen(PORT, ()=>{    
+    console.log(`API server now on port 3001`);
+});
